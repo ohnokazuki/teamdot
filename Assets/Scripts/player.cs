a57dbd697d79b96d5f164c95252984741820a432
hello30190f <w5w5ww6w6w@gmail.com>
2023-05-23 19:21:53 +0900

    wasd キーでプレイヤーが動けるようにしました

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -22,16 +22,16 @@ public class player : MonoBehaviour
 
     void Operation()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
             transform.position += transform.up * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
             transform.position -= transform.up * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             transform.position += transform.right * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             transform.position -= transform.right * speed * Time.deltaTime;
     }
 
