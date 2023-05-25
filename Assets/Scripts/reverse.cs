dd3e92eec3c7ac579c78b86ef4411c85cecd43ca
ohnokazuki <83818551+ohnokazuki@users.noreply.github.com>
2023-05-25 18:46:26 +0900

    unity
    反転

--- Assets/Scripts/reverse.cs
+++ Assets/Scripts/reverse.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletmaterial : MonoBehaviour
+public class reverse : MonoBehaviour
 {
-    public GameObject bulletprefab;
-    float speed;
-
+    public float point;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 10.0f;  // 弾の速度
+        
     }
 
     // Update is called once per frame
@@ -24,12 +22,16 @@ public class bulletmaterial : MonoBehaviour
             // 向きの生成（Z成分の除去と正規化）
             Vector3 shotForward = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
 
-            // 弾（ゲームオブジェクト）の生成
-            GameObject clone = Instantiate(bulletprefab, transform.position+(shotForward*3), Quaternion.identity);
-
-            // 弾に速度を与える
-            clone.GetComponent<Rigidbody2D>().velocity = shotForward * speed;
-
+            if (shotForward.x < 0 & point > 0)
+            {
+                this.transform.eulerAngles = new Vector3(0, 0, 0);
+                point = shotForward.x;
+            }
+            else if (shotForward.x > 0 & point < 0)
+            {
+                this.transform.eulerAngles = new Vector3(0, 180, 0);
+                point = shotForward.x;
+            }
         }
     }
 }
