2205078278038c8ea1e574745fa447ad4b7bab05
hello30190f <w5w5ww6w6w@gmail.com>
2023-06-06 18:26:32 +0900

    Fix setting screen

--- Assets/Scripts/settings/changePlayerSpeed.cs
+++ Assets/Scripts/settings/changePlayerSpeed.cs
@@ -10,18 +10,18 @@ public class changePlayerSpeed : MonoBehaviour
         player.speed = slider.value;
         Debug.Log("Player speed is changed to " +  player.speed);
 
-        //Find showPlayerSpeed and get text component.
-        //After that, set text to player speed
-        GameObject []showSpeed = GameObject.FindGameObjectsWithTag("UI");
-        foreach (GameObject go in showSpeed)
-        {
-            if(go.name == "showPlayerSpeed")
-            {
-                Text speedText = go.GetComponent<Text>();
-                speedText.text = player.speed.ToString();
-                Debug.Log("Set value correctly");
-            }
-        }
+        ////Find showPlayerSpeed and get text component.
+        ////After that, set text to player speed
+        //GameObject []showSpeed = GameObject.FindGameObjectsWithTag("UI");
+        //foreach (GameObject go in showSpeed)
+        //{
+        //    if(go.name == "showPlayerSpeed")
+        //    {
+        //        Text speedText = go.GetComponent<Text>();
+        //        speedText.text = player.speed.ToString();
+        //        Debug.Log("Set value correctly");
+        //    }
+        //}
 
     }
 }
