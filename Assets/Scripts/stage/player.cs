58e84dba0b4a7b3e3e8df8a9dc5e81349cb22e6a
hello30190f <108202763+hello30190f@users.noreply.github.com>
2023-07-07 16:22:03 +0900

    prepare fixing player

--- Assets/Scripts/stage/player.cs
+++ Assets/Scripts/stage/player.cs
@@ -87,6 +87,23 @@ public class player : MonoBehaviour
         }
     }
 
+    void OperationRigBodyEditon(){
+        if (movable)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+                transform.position += transform.up * speed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+                transform.position -= transform.up * speed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                transform.position += transform.right * speed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+
     void slowMotion()
     {
         if (Input.GetKey(KeyCode.F))
