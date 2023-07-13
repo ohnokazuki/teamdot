1c4488091bcdc762feb31b54d0c71ac430d64ca5
hello30190f <w5w5ww6w6w@gmail.com>
2023-07-13 20:25:18 +0900

    adjust

--- Assets/Scripts/stage/openDoor.cs
+++ Assets/Scripts/stage/openDoor.cs
@@ -13,19 +13,36 @@ public class openDoor : MonoBehaviour
     [SerializeField]
     TileBase nothing;
 
+    [SerializeField]
+    GameObject Grid;
+
     Vector3Int target;
     Vector3 Acollider;
+
+    [SerializeField]
+    float scale;
+    [SerializeField]
+    float Unit;
 
     private void Start()
     {
         //doorTile = gameObject.GetComponent<Tilemap>();
     }
 
+    Vector3 convertTileMapPosition(Vector3 playerPosition)
+    {
+        // 0,0,0
+        // get grid position
+        Vector3 GridTrans = Grid.transform.position;
+        Vector3 tileMapPosition = new Vector3(playerPosition.x + GridTrans.x,playerPosition.y + GridTrans.y,playerPosition.z + GridTrans.z);
+        return tileMapPosition;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("hey guys! I'm tile map collider!");
         Acollider = collision.transform.position;
-        Acollider = Camera.main.ScreenToWorldPoint(Acollider);
+        Acollider = convertTileMapPosition(Acollider);
         target.x = (int)Acollider.x; target.y = (int)Acollider.y + 1; target.z = (int)Acollider.z;
         Debug.Log("You touched " + target);
         Debug.Log("collision args " + Acollider);
